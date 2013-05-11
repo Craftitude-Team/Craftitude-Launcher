0dbac1be8836cf5b1b4f6946e7baadfefb0bcfb8
icedream <icedream2k9@die-optimisten.net>
2013-05-11 22:11:10 +0200

    Put cache for Craftitude into a subfolder of the base.

--- Craftitude-Api/Client.cs
+++ Craftitude-Api/Client.cs
@@ -10,22 +10,69 @@ namespace Craftitude
     {
         public string BasePath { get; set; }
         public Cache Cache { get; set; }
+        public List<Repository> Repositories { get; set; }
 
         public Client(string basePath)
         {
             if(!Directory.Exists(basePath))
                 Directory.CreateDirectory(basePath);
-
             this.BasePath = Path.GetFullPath(basePath);
-            this.Cache = new Cache(this, Path.Combine(this.BasePath, "craftitude.cache"));
+
+            string craftitudeBasePath = Path.Combine(basePath, ".craftitude");
+            if(!Directory.Exists(craftitudeBasePath))
+                Directory.CreateDirectory(craftitudeBasePath);
+
+            this.Cache = new Cache(this, Path.Combine(craftitudeBasePath, "craftitude.cache"));
+
+            // Load repositories
+            string repositoriesConf = Path.Combine(craftitudeBasePath, "repositories.conf");
+            if (!File.Exists(repositoriesConf))
+            {
+                using (var f = File.CreateText(repositoriesConf))
+                {
+                    f.WriteLine("# You need to put all Craftitude repositories you want to use here.");
+                    f.WriteLine("# All repositories which are put here should be added in this form:");
+                    f.WriteLine("#");
+                    f.WriteLine("# repository crep://repository.serv.er/path/to/repository distribution another-optional-distribution");
+                    f.WriteLine("#");
+                    f.WriteLine("# Example:");
+                    f.WriteLine("#");
+                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications client-1.5.1 server-1.5.1");
+                    f.WriteLine("#");
+                    f.WriteLine("# ...would be the same as...");
+                    f.WriteLine("#");
+                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications client-1.5.1");
+                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications server-1.5.1");
+                    f.WriteLine("#");
+                    f.Flush();
+                }
+            }
+            else
+            {
+                using (var f = File.OpenText(repositoriesConf))
+                {
+                    while (!f.EndOfStream)
+                    {
+                        var line = f.ReadLine();
+                        if (Repository.IsValidConfigString(line))
+                            this.Repositories.AddRange(Repository.FromConfigString(line, this));
+                    }
+                }
+            }
+        }
+
+        public void UpdateCache()
+        {
+            foreach (var repo in Repositories)
+            {
+                
+            }
         }
 
         public bool IsInstalled(string packageID)
         {
             return Cache.GetInstalledPackagesCache().GetPackage(packageID) != null;
         }
-
-        // TODO: Implement client functions
 
         public void Install(string packageID)
         {
@@ -41,6 +88,7 @@ namespace Craftitude
 
         public RepositoryPackage GetOnlinePackageInfo(string packageID)
         {
+
         }
 
         public InstalledPackage GetOfflinePackageInfo(string packageID)

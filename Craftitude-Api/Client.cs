823eb9c6721e14e66749661552e8f9bceb2898e1
icedream <icedream2k9@die-optimisten.net>
2013-05-13 22:58:40 +0200

    Implementation of status "operations".

--- Craftitude-Api/Client.cs
+++ Craftitude-Api/Client.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using YaTools.Yaml;
 
 namespace Craftitude
 {
@@ -10,7 +15,27 @@ namespace Craftitude
     {
         public string BasePath { get; set; }
         public Cache Cache { get; set; }
-        public List<Repository> Repositories { get; set; }
+        public List<Repository> Repositories { get; private set; }
+        public ObservableCollection<Operation> Operations { get; set; } // TODO: Make operations list read-only to public
+
+        private int nextOperationIndexToAssign = 0;
+
+        /*
+
+        public event EventHandler<OperationUpdateEventArgs> OperationUpdate;
+
+        internal void OnOperationUpdate(OperationUpdateEventArgs e)
+        {
+            if (OperationUpdate != null)
+                OperationUpdate.Invoke(this, e);
+        }
+
+        internal void OnOperationUpdate(Operation e)
+        {
+            OnOperationUpdate(new OperationUpdateEventArgs(e));
+        }
+        
+         */
 
         public Client(string basePath)
         {
@@ -25,27 +50,12 @@ namespace Craftitude
             this.Cache = new Cache(this, Path.Combine(craftitudeBasePath, "craftitude.cache"));
 
             // Load repositories
+            this.Repositories = new List<Repository>();
             string repositoriesConf = Path.Combine(craftitudeBasePath, "repositories.conf");
             if (!File.Exists(repositoriesConf))
             {
                 using (var f = File.CreateText(repositoriesConf))
-                {
-                    f.WriteLine("# You need to put all Craftitude repositories you want to use here.");
-                    f.WriteLine("# All repositories which are put here should be added in this form:");
-                    f.WriteLine("#");
-                    f.WriteLine("# repository crep://repository.serv.er/path/to/repository distribution another-optional-distribution");
-                    f.WriteLine("#");
-                    f.WriteLine("# Example:");
-                    f.WriteLine("#");
-                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications client-1.5.1 server-1.5.1");
-                    f.WriteLine("#");
-                    f.WriteLine("# ...would be the same as...");
-                    f.WriteLine("#");
-                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications client-1.5.1");
-                    f.WriteLine("# repository crep://craftitude.customserver.com/modifications server-1.5.1");
-                    f.WriteLine("#");
                     f.Flush();
-                }
             }
             else
             {
@@ -59,12 +69,46 @@ namespace Craftitude
                     }
                 }
             }
+
+            // Operations collection
+            this.Operations = new ObservableCollection<Operation>();
+        }
+
+        internal int GetOperationID()
+        {
+            int newOperationIndex = nextOperationIndexToAssign;
+            nextOperationIndexToAssign++;
+            return newOperationIndex;
         }
 
         public void UpdateCache()
         {
+            var thisOperation = new CollectiveOperation(this) { Text = Properties.Resources.FetchingUpdatesFromRepositories };
+
+            var operationAssignments = new Dictionary<CollectiveOperation, Repository>();
             foreach (var repo in Repositories)
             {
+                var o = new CollectiveOperation(this);
+                o.Text = repo.Url;
+                o.Suboperations.Add(new ProgressingOperation(this) { Text = Properties.Resources.DownloadingRepositoryUpdates });   // 0: Downloading package list
+                o.Suboperations.Add(new ProgressingOperation(this) { Text = Properties.Resources.MergingRepositoryUpdates });       // 1: Merging package list
+                thisOperation.Suboperations.Add(o);
+                operationAssignments.Add(o, repo);
+            }
+
+            foreach (var operation in operationAssignments.Keys)
+            {
+                string packagesYml = string.Empty;
+                var repository = operationAssignments[operation];
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Disposed += (s, e) => { ((ProgressingOperation)operation.Suboperations[0]).Progress = 1; };
+                    wc.DownloadProgressChanged += (s, e) => { ((ProgressingOperation)operation.Suboperations[0]).Progress = e.ProgressPercentage / 100; };
+                    packagesYml = wc.DownloadString(repository.GetPackagesYmlUrl());
+                }
+
+                var packageList = YamlLanguage.StringTo(packagesYml);
+
                 
             }
         }
---
+++ Craftitude-Api/Operation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Craftitude
+{
+    public class Operation
+    {
+        public int ID { get; private set; }
+        public Client Client { get; private set; }
+        public string Text { get; internal set; }
+
+        public Operation(Client client)
+        {
+            this.ID = client.GetOperationID();
+            this.Client = client;
+        }
+    }
+
+    public class CollectiveOperation : Operation
+    {
+        public List<Operation> Suboperations { get; internal set; }
+
+        public double Progress
+        {
+            get
+            {
+                return Suboperations.OfType<ProgressingOperation>().Average(p => p.Progress);
+            }
+        }
+
+        public CollectiveOperation(Client client)
+            : base(client)
+        {
+            this.Suboperations = new List<Operation>();
+        }
+    }
+
+    public class ProgressingOperation : Operation
+    {
+        public double Progress { get; internal set; }
+
+        public ProgressingOperation(Client client)
+            : base(client)
+        {
+            Progress = 0;
+        }
+    }
+
+    /*
+    public class OperationUpdateEventArgs
+    {
+        public Operation Operation { get; private set; }
+
+        public OperationUpdateEventArgs(Operation operation)
+        {
+            this.Operation = operation;
+        }
+    }
+     */
+
+}

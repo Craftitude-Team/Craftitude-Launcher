27dc0a870f0152b8989613c0d5dc1936031871e6
icedream <icedream2k9@die-optimisten.net>
2013-05-25 20:41:04 +0200

    Fix for init-git tool: Recursive clone.

--- tools/init-git.bat
+++ tools/init-git.bat
@@ -5,10 +5,10 @@ set target=%name%
 
 if not exist "%target%\.git\index" (
 	xecho /a:%col_stat% "Downloading library %name%..."
-	git clone -v %url% "%target%" |xecho /f:"\t{}" /a:%col_proc%
+	git clone --recursive -v %url% "%target%" |xecho /f:"\t{}" /a:%col_proc%
 ) else (
 	xecho /a:%col_stat% "Updating library %name%..."
 	pushd "%target%"
-	git pull --rebase -v |xecho /f:"\t{}" /a:%col_proc%
+	git pull --recursive --rebase -v |xecho /f:"\t{}" /a:%col_proc%
 	popd
 )
